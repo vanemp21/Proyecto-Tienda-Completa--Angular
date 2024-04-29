085b3a3d042e20040d695c3bb79d8ce127fbac8e
Vanessa <vanemp21@gmail.com>
2024-04-29 18:08:27 +0200

    feat(create): Create structure components and firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,30 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
+import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getAuth, provideAuth} from '@angular/fire/auth'
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp(environment))),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideAuth(()=>getAuth())),
+    provideAnimations(), 
+    provideToastr({
+      timeOut: 2000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    }), 
+
+
+    
+  ],
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+import { FirebaseOptions } from "@angular/fire/app";
+
+export const environment: FirebaseOptions = {
+  projectId: 'tienda-app-81e83',
+  appId: '1:610882912341:web:9f2ab2b97b0c6bc54b54dc',
+  storageBucket: 'tienda-app-81e83.appspot.com',
+  apiKey: 'AIzaSyCYM7OxGV-kwPSdAKhu9IGIpJjolf_HzHg',
+  authDomain: 'tienda-app-81e83.firebaseapp.com',
+  messagingSenderId: '610882912341'
+};
